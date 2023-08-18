d8d525612d03fefe3ee5085616faac0d15a6e674
mdraselislam1944 <mdraselislam1944@gmail.com>
2023-08-18 16:18:25 +0600

    add-jwt

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const stripe=require('stripe')('sk_test_51NEmG3IxzytApYUlezdVCVvSiGKYTMPRcizhPcJbk70FNEUHtQQ4Zo6Oypdn7Jmpir3PLHlhMOx0zLRuvL0dzqhA00ZSSQyNYP')
+const stripe = require('stripe')('sk_test_51NEmG3IxzytApYUlezdVCVvSiGKYTMPRcizhPcJbk70FNEUHtQQ4Zo6Oypdn7Jmpir3PLHlhMOx0zLRuvL0dzqhA00ZSSQyNYP')
 
 
 const app = express();
@@ -43,55 +44,80 @@ app.get('/', (req, res) => {
 })
 ///start writing method from here .
 //Please don't override the code .
+//----------------------------------------jwt start -------------------------------------------------------------------------
+
+app.post('/jwt', (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+  // console.log(user)
+  res.send({ token });
+})
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  // console.log(authorization)
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(403).send({ error: true, message: 'unauthorized access' });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
+//----------------------------------------jwt end -------------------------------------------------------------------------
 //------------------------------------------payment start from here----------------------------------------------------------
- 
+
 const { ObjectId } = require('mongodb');
 
 
-app.post('/create-payment-intent',async(req,res)=>{
-  const {price}=req.body;
-  const amount=price*100;
-  const paymentIntent=await stripe.paymentIntents.create({
-      amount:amount,
-      currency:'usd',
-      payment_method_types:['card']
+app.post('/create-payment-intent', async (req, res) => {
+  const { price } = req.body;
+  const amount = price * 100;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card']
   })
   res.send({
-      clientSecret:paymentIntent.client_secret
+    clientSecret: paymentIntent.client_secret
   })
 })
 
 // const paymentHistory= client.db('toyBabyDB').collection('paymentHistory');
-const paymentHistory= client.db('Crowd-funding').collection('Payment');
-app.post("/saveAddress",async(req,res)=>{
-    const data=req.body;
-    console.log(data);
-    const result=await paymentHistory.insertOne(data);
-    res.send(result);
+const paymentHistory = client.db('Crowd-funding').collection('Payment');
+app.post("/saveAddress", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  const result = await paymentHistory.insertOne(data);
+  res.send(result);
 
 })
 
-app.get("/saveAddress/:id",async(req,res)=>{
-    const id=req.params.id;
-    const result=await paymentHistory.findOne({ _id: new ObjectId(id) });
-    res.send(result);
+app.get("/saveAddress/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await paymentHistory.findOne({ _id: new ObjectId(id) });
+  res.send(result);
 })
 
-app.patch('/payment/saveAddress/:id',async(req,res)=>{
-    const id=req.params.id;
-    const paymentSuccess=req.body;
-    const filter={_id: new ObjectId(id)}
-    const option={upsert:true}
-    const updateStudent={
-      $set:{
-        transaction:paymentSuccess.transaction,
-      }
+app.patch('/payment/saveAddress/:id', async (req, res) => {
+  const id = req.params.id;
+  const paymentSuccess = req.body;
+  const filter = { _id: new ObjectId(id) }
+  const option = { upsert: true }
+  const updateStudent = {
+    $set: {
+      transaction: paymentSuccess.transaction,
     }
-    const result=await paymentHistory.updateOne(filter,updateStudent,option);
-    res.send(result);
-  })
+  }
+  const result = await paymentHistory.updateOne(filter, updateStudent, option);
+  res.send(result);
+})
 
 //------------------------------------------payment end here----------------------------------------------------------
 
@@ -112,10 +138,10 @@ const userHistory = client.db("Crowd-funding").collection("User");
 app.post('/users', async (req, res) => {
   const user = req.body;
   console.log(user);
-  const query ={email : user.email}
-  const existingUser =await userHistory.findOne(query);
-  if(existingUser){
-    return res.send({message:'already exists'})
+  const query = { email: user.email }
+  const existingUser = await userHistory.findOne(query);
+  if (existingUser) {
+    return res.send({ message: 'already exists' })
   }
   const result = await userHistory.insertOne(user);
   res.send(result);
