101fc4d33f582af579bdee8ed73606c4f5810a95
mdraselislam1944 <mdraselislam1944@gmail.com>
2023-08-23 22:17:10 +0600

    add-verify-admin

--- index.js
+++ index.js
@@ -71,6 +71,26 @@ const verifyJWT = (req, res, next) => {
 
 
 //----------------------------------------jwt end -------------------------------------------------------------------------
+
+
+
+
+
+//----------------------------------------------Admin Verify implementation start ------------------------------------------
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email }
+  const user = await usersCollection.findOne(query);
+  if (user?.role !== 'admin') {
+    return res.status(403).send({ error: true, message: 'forbidden message' });
+  }
+  next();
+}
+
+
+//----------------------------------------------Admin Verify implementation end ------------------------------------------
+
 //------------------------------------------payment start from here----------------------------------------------------------
 
 const { ObjectId } = require('mongodb');
