6f1ae5a23032d59689667ca845a480f298b584ab
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-08-31 21:06:54 +0600

    change the name

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const verifyJWT = (req, res, next) => {
 const verifyAdmin = async (req, res, next) => {
   const email = req.decoded.email;
   const query = { email: email }
-  const user = await usersCollection.findOne(query);
+  const user = await userCollection.findOne(query);
   if (user?.role !== 'admin') {
     return res.status(403).send({ error: true, message: 'forbidden message' });
   }
