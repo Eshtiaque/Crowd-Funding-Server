6e1accbd4f43e502279de0ab4802fd3f0158c537
mdraselislam1944 <mdraselislam1944@gmail.com>
2023-09-07 13:06:10 +0600

    'add-admin'

--- index.js
+++ index.js
@@ -88,6 +88,19 @@ const verifyAdmin = async (req, res, next) => {
   next();
 }
 
+app.get('/users/admin/:email',verifyJWT, async (req, res) => {
+  const email = req.params.email;
+
+  if (req.decoded.email !== email) {
+    res.send({ admin: false })
+  }
+
+  const query = { email: email }
+  const user = await userCollection.findOne(query);
+  const result = { admin: user?.role === 'admin' }
+  res.send(result);
+})
+
 
 //----------------------------------------------Admin Verify implementation end ------------------------------------------
 
